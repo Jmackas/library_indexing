5e8f8ee89c0ce6478840f6011c20b3eb18b7d615
Jmackas <jamesrossmackay@gmail.com>
2017-07-07 11:12:19 +0800

    Update

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+
+// Copy and paste button
+
+                    function copyToClipboard(element) {
+                        var $temp = $("<input>");
+                        $("body").append($temp);
+                        $temp.val($(element).text()).select();
+                        document.execCommand("copy");
+                        $temp.remove();
+                    }
+
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
